530bfe074a7db52f0bba06ea39c5e31f18d22f8c
Arun Krishna <arunpradeep585@gmail.com>
2026-07-05 22:08:08 +0530

    Fix : Vercel push

--- next.config.ts
+++ next.config.ts
@@ -16,6 +16,10 @@ const nextConfig: NextConfig = {
         hostname: "dtcbackend-production-036b.up.railway.app",
       },
       {
+        protocol: "https",
+        hostname: "dtcbackend-production-4bfb.up.railway.app",
+      },
+      {
         protocol: "http",
         hostname: "localhost",
         port: "9000",
